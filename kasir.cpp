c38526a42de7d5c20e54d9c90f81059346b54f46
Muthi'ah <89143710+heyhooooooooooooooooo@users.noreply.github.com>
2022-09-30 08:41:03 +0700

    Add files via upload

---
+++ kasir.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+ using namespace std;
+ main() {
+     string nama = "hikmah muthiah" ;
+     string NIM = " 422021618033" ;
+     int diskon, i , j , x, total;
+     int bayar (10), harga (10), jbrng (10), totali= 0 ;
+
+
+
+     cout <<nama<<endl;
+     cout <<NIM<<endl;
+     cout<<"====================="<<endl;
+     cout<<"program kasir"<<endl;
+     cout<<"====================="<<endl;
+     cout<<endl;
+     cout<<"masukkan jumlah data=" ;
+     cin>>x;
+     cout<<endl;
+
+     for (i=0;i <x;i++) ;{
+         cout<<"=========================="<<endl;
+         cout<<endl;
+         cout<<"masukkan nama barang=" ;
+         cin>>nama (x);
+         cout<<"masukkan harga barang= RP. ";
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
